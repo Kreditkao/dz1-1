d89864a70cb386f895b683abb475278aa4e1f5ad
Kreditkao <bermutok@gmail.com>
2025-05-10 17:00:59 +0300

    Create Student.cs

---
+++ Models/Student.cs
@@ -0,0 +1,9 @@
+namespace ConsoleApp2.Models;
+
+public class Student
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required int Age { get; set; }
+    public required string Email { get; set; }
+}
